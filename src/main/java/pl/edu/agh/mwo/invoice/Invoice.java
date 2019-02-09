b8e5770549fb3aa7c304897c6423799b8ea623e6
student19 <student19@pcoip.ki.agh.edu.pl>
2019-02-09 12:17:03 +0100

    testy prod gotowe

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,42 @@
 package pl.edu.agh.mwo.invoice;
-
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	private Collection<Product> products = new ArrayList<Product>();
 
 	public void addProduct(Product product) {
+		this.products.add(product);
 		// TODO: implement
 	}
 
 	public void addProduct(Product product, Integer quantity) {
+		if (quantity<=0|| quantity <0){
+			throw new IllegalArgumentException("quantity ==0");
+		}
+		for (int i=0; i < quantity; i++){
+			this.products.add(product);
+		}
 		// TODO: implement
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getTotalNetPrice() {
+		// return BigDecimal.ZERO;
+		BigDecimal sum = BigDecimal.ZERO;
+		for (Product product : this.products){
+			sum = sum.add(product.getTotalGrossPrice());
+		}
+		return sum;
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		return BigDecimal.ZERO;
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getTotalGrossPrice() {
+		return BigDecimal.ZERO;
 	}
 }
